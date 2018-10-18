554c50d90f87a2596a2a39c8440b3d4978bea264
Thomas Cort <linuxgeek@gmail.com>
2018-10-18 13:39:13 -0400

    Initial Import

---
+++ javascript-oddities.js
@@ -0,0 +1,46 @@
+http = $http$ = require('http');
+
+global['console'][(`l${"".substr(0)}o${''.substring(0)}g`)]('5' + (function Function() {
+
+    i = new Number("0O0")
+      + new Number("\r\n\t")
+      + new Number("0X0")
+      ;
+
+    a = {
+        i: i,
+        valueOf: () => ++i,
+    }
+
+    if (a == 1 && a == 2 && a == 3 && +null === 0) {
+        i -= 3;
+    }
+
+/**//**//**//**//**//**//**//**//**/
+/**/ https://www.w3schools.com/ /**/
+/**//**//**//**//**//**//**//**//**/
+
+    for (var let = i++ + ++i; [] == ![] && +[] == -[] ; let += true) {
+        if (0) let; else switch (let) {
+            case Array.apply(null, (a,b,c) => {}) . length : break;
+            default: continue https;//www.w3schools.com/
+            case 4: try { return let; } finally { return let + 1; };
+        }
+    }
+
+    if ( ( 0.3 - (0.2 + 0.1) ) == ( 0.3 - 0.2 - 0.1 ) ) {
+        return 0xBeefCafe;
+    }
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; return let, i = 42; ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    do {
+        var i = 1337;
+    } while (i-->42); // arrow operator
+
+    return X=x=x=X;
+
+})() - '\r'.charCodeAt([] == []));
+
